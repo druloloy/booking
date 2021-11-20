6567e80f640262410ab2323aabf0dadb0a68cf4d
druloloy <dru.loloy@gmail.com>
2021-11-20 22:38:36 +0800

    [MOD] Added new functions to suit the API's requirement

--- utils/ManageDates.js
+++ utils/ManageDates.js
@@ -3,6 +3,7 @@ class ManageDates {
     this.year = year;
     this.month = month;
   }
+
   #getMonthNumber = (month = this.month) => {
     const months = [
       "January",
@@ -21,7 +22,7 @@ class ManageDates {
     return months.indexOf(month) + 1;
   };
   #getAllDays = (year = this.year, month = this.month) => {
-    const monthNumber = getMonthNumber();
+    const monthNumber = this.#getMonthNumber();
     const dateInput = new Date(year, monthNumber, 0);
     return dateInput.getDate();
   };
@@ -32,48 +33,62 @@ class ManageDates {
     const dateArray = [];
 
     for (let i = 1; i <= days; i++) {
-      const date = new Date(year, month - 1, i);
+      const date = new Date(year, month-1, i);
       dateArray.push(date);
     }
     return dateArray;
   };
   /**
    *
-   * @param {Array} occupied
+   * @param {Date[]} occupied
    * @param {Number} year
    * @param {String | Number} month
    * @returns Date[] - Array of dates that are not occupied in the given year and month.
    */
-  getAvailableDates = function (
-    occupied,
-    year = this.year,
-    month = this.month
-  ) {
+  getAvailableDates = function (occupied, year = this.year, month = this.month) {
+
     month = this.#getMonthNumber(month);
-    const occupiedDays = occupied.map((date) => new Date(date).getDate());
-    const availableDays = this.getDateArray();
-    return availableDays
-      .filter((date) => {
-        return !occupiedDays.includes(new Date(date).getDate());
-      })
-      .map((date) => {
-        return new Date(year, month, date);
-      });
+    const availableDays = this.getDateArray().map((d)=>{
+        return new Date(d).toLocaleDateString('default', {
+            dateStyle: 'full'
+        });
+    })
+    occupied = occupied.map((d)=>{
+        return new Date(d).toLocaleDateString('default', {
+            dateStyle: 'full'
+        });
+    });
+
+    return availableDays.filter((date) => {
+      return !occupied.includes(date);
+    });
   };
   /**
    *
-   * @param {Array} occupied
+   * @param {Date[]} occupied
    * @param {Number} year
    * @param {String | Number} month
    * @returns Date[] - Array of days that are not occupied in the given year and month.
    */
   getAvailableDays = function (occupied, year = this.year, month = this.month) {
     month = this.#getMonthNumber(month);
-    const occupiedDays = occupied.map((date) => new Date(date).getDate());
-    const availableDays = this.getDateArray();
+    const occupiedDays = occupied.map((date) => date.getDate());
+    const availableDays = this.getDateArray().map(d=>d.getDate());
     return availableDays.filter((date) => {
-      return !occupiedDays.includes(new Date(date).getDate());
+      return !occupiedDays.includes(date);
     });
   };
+
+
+  startDay = () => 1;
+  endDay = function () {
+    return this.#getAllDays();
+  };
+  startDate = function () {
+    return new Date(this.year, this.#getMonthNumber()-1, this.startDay());
+  };
+  endDate = function () {
+    return new Date(this.year, this.#getMonthNumber()-1, this.endDay());
+  };
 }
-modules.export = ManageDates;
+module.exports = ManageDates;
